e8eea8e611f6c932170425db3d14f4d8b00f32fc
Abossjjjj <MaySsonu6@gmail.com>
2024-09-15 02:21:47 +0300

    Update index.js

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ app.get('/', (req, res) => {
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
+
 // Ping to keep the server alive
 setInterval(() => {
     http.get('https://stream-denim-regnosaurus.glitch.me/:' + PORT);
