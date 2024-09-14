61927a0eb4ec433a63ec4c6772c0155e672e5c44
Abossjjjj <MaySsonu6@gmail.com>
2024-09-15 01:32:26 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,191 +1,143 @@
 const axios = require('axios');
+const fetch = require('node-fetch');
 const { v4: uuidv4 } = require('uuid');
-const crypto = require('crypto');
 const fs = require('fs');
-const readline = require('readline');
 const FormData = require('form-data');
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
+// ضع التوكن الخاص بالبوت هنا
+const token = '7463685279:AAEtkWr1SqOQzUL5VAPK_rF-ZLtzEuO-pVQ';
+
+// ضع الـ ID الخاص بالمستخدم هنا
+const ID = '7193004338';
 
 const uid = uuidv4();
-const csr = crypto.randomBytes(8).toString('hex').repeat(2);
+const csr = [...Array(16)].map(() => Math.floor(Math.random() * 16).toString(16)).join('');
 
-function generateUserAgent() {
-  return 'Instagram 100.0.0.17.129 Android (29/10; 420dpi; 1080x2129; samsung; SM-M205F; m20lte; exynos7904; en_GB; 161478664)';
-}
+// طلب البريد الإلكتروني عبر البوت
+const sendMessage = async (token, chat_id, text) => {
+    const url = `https://api.telegram.org/bot${token}/sendMessage`;
+    await axios.post(url, { chat_id, text });
+};
 
-async function sendMessage(token, chatId, text) {
-  const url = `https://api.telegram.org/bot${token}/sendMessage`;
-  return axios.post(url, { chat_id: chatId, text: text });
-}
+const getUpdates = async (token) => {
+    const url = `https://api.telegram.org/bot${token}/getUpdates`;
+    const response = await axios.get(url);
+    return response.data;
+};
 
-async function getUpdates(token) {
-  const url = `https://api.telegram.org/bot${token}/getUpdates`;
-  return axios.get(url);
-}
+// طلب البريد الإلكتروني من المستخدم
+const runBot = async () => {
+    await sendMessage(token, ID, "Please enter your email:");
 
-async function GetInfoInsta(username) {
-  // Implement Instagram info gathering logic here
-  // This is a placeholder and should be replaced with actual implementation
-  return {
-    id: '12345678',
-    followers: '1000',
-    following: '500',
-    posts: '100'
-  };
-}
-
-async function main() {
-  const token = await new Promise(resolve => rl.question('Enter Token: ', resolve));
-  const ID = await new Promise(resolve => rl.question('Enter ID: ', resolve));
-
-  await sendMessage(token, ID, "Please enter your email:");
-
-  let email = "";
-  while (!email) {
-    const updates = await getUpdates(token);
-    if (updates.data.result && updates.data.result.length > 0) {
-      const lastMessage = updates.data.result[updates.data.result.length - 1].message;
-      email = lastMessage.text;
+    // الانتظار للحصول على رد المستخدم
+    let email = "";
+    while (!email) {
+        const updates = await getUpdates(token);
+        if (updates.result && updates.result.length > 0) {
+            const lastMessage = updates.result[updates.result.length - 1].message;
+            email = lastMessage.text;
+        }
+        await new Promise(resolve => setTimeout(resolve, 2000));
     }
-    await new Promise(resolve => setTimeout(resolve, 2000));
-  }
 
-  const headers = {
-    'X-Pigeon-Session-Id': '50cc6861-7036-43b4-802e-fb4282799c60',
-    'X-Pigeon-Rawclienttime': '1700251574.982',
-    'X-IG-Connection-Speed': '-1kbps',
-    'X-IG-Bandwidth-Speed-KBPS': '-1.000',
-    'X-IG-Bandwidth-TotalBytes-B': '0',
-    'X-IG-Bandwidth-TotalTime-MS': '0',
-    'X-Bloks-Version-Id': '009f03b18280bb343b0862d663f31ac80c5fb30dfae9e273e43c63f13a9f31c0',
-    'X-IG-Connection-Type': 'WIFI',
-    'X-IG-Capabilities': '3brTvw==',
-    'X-IG-App-ID': '567067343352427',
-    'User-Agent': generateUserAgent(),
-    'Accept-Language': 'en-GB, en-US',
-    'Cookie': 'mid=ZVfGvgABAAGoQqa7AY3mgoYBV1nP; csrftoken=9y3N5kLqzialQA7z96AMiyAKLMBWpqVj',
-    'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-    'Accept-Encoding': 'gzip, deflate',
-    'Host': 'i.instagram.com',
-    'X-FB-HTTP-Engine': 'Liger',
-    'Connection': 'keep-alive',
-    'Content-Length': '356',
-  };
+    const headers = {
+        'X-Pigeon-Session-Id': '50cc6861-7036-43b4-802e-fb4282799c60',
+        'X-Pigeon-Rawclienttime': '1700251574.982',
+        'X-IG-Connection-Speed': '-1kbps',
+        'X-IG-Bandwidth-Speed-KBPS': '-1.000',
+        'X-IG-Bandwidth-TotalBytes-B': '0',
+        'X-IG-Bandwidth-TotalTime-MS': '0',
+        'X-Bloks-Version-Id': '009f03b18280bb343b0862d663f31ac80c5fb30dfae9e273e43c63f13a9f31c0',
+        'X-IG-Connection-Type': 'WIFI',
+        'X-IG-Capabilities': '3brTvw==',
+        'X-IG-App-ID': '567067343352427',
+        'User-Agent': 'Instagram 100.0.0.17.129 Android (29/10; 420dpi; 1080x2129; samsung; SM-M205F; m20lte; exynos7904; en_GB; 161478664)',
+        'Accept-Language': 'en-GB, en-US',
+        'Cookie': `mid=ZVfGvgABAAGoQqa7AY3mgoYBV1nP; csrftoken=9y3N5kLqzialQA7z96AMiyAKLMBWpqVj`,
+        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+        'Accept-Encoding': 'gzip, deflate',
+        'Host': 'i.instagram.com',
+        'X-FB-HTTP-Engine': 'Liger',
+        'Connection': 'keep-alive',
+        'Content-Length': '356',
+    };
 
-  const data = {
-    signed_body: `0d067c2f86cac2c17d655631c9cec2402012fb0a329bcafb3b1f4c0bb56b1f1f.{"_csrftoken":"9y3N5kLqzialQA7z96AMiyAKLMBWpqVj","adid":"${uuidv4()}","guid":"${uuidv4()}","device_id":"${uuidv4()}","query":"${email}"}`,
-    ig_sig_key_version: '4',
-  };
-
-  const res = await axios.post('https://i.instagram.com/api/v1/accounts/send_recovery_flow_email/', data, { headers });
-  const rest = res.data.status === 'ok' ? res.data.email : 'Band Requests!';
-
-  const user = email.split('@')[0];
-  const heada = {
-    "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
-    "Host": "i.instagram.com",
-    "Connection": "Keep-Alive",
-    "User-Agent": generateUserAgent(),
-    "Cookie": `mid=YwvCRAABAAEsZcmT0OGJdPu3iLUs; csrftoken=${csr}`,
-    "Cookie2": "$Version=1",
-    "Accept-Language": "en-US",
-    "X-IG-Capabilities": "AQ==",
-    "Accept-Encoding": "gzip",
-  };
-
-  const datai = {
-    q: user,
-    device_id: `android${uid}`,
-    guid: uid,
-    _csrftoken: csr
-  };
-
-  const kid = await axios.post('https://i.instagram.com/api/v1/users/lookup/', datai, { headers: heada });
-  const kidData = kid.data;
-
-  let id, prv, ph, sms, fb, wa, phn, name, profile_pic_url;
-  try {
-    id = kidData.user.pk;
-    prv = kidData.user.is_private;
-    ph = kidData.has_valid_phone;
-    sms = kidData.can_sms_reset;
-    fb = kidData.fb_login_option;
-    wa = kidData.can_wa_reset;
-    phn = kidData.obfuscated_phone;
-    name = kidData.user.full_name;
-    profile_pic_url = kidData.user.profile_pic_url;
-  } catch (error) {
-    console.error('Error extracting data from kidData:', error);
-  }
-
-  const profile_pic_path = `${user}.jpg`;
-  if (profile_pic_url) {
+    const data = `signed_body=0d067c2f86cac2c17d655631c9cec2402012fb0a329bcafb3b1f4c0bb56b1f1f.{"_csrftoken":"9y3N5kLqzialQA7z96AMiyAKLMBWpqVj","adid":"${uuidv4()}","guid":"${uuidv4()}","device_id":"${uuidv4()}","query":"${email}"}`;
+    
+    let rest;
     try {
-      const response = await axios.get(profile_pic_url, { responseType: 'arraybuffer' });
-      fs.writeFileSync(profile_pic_path, response.data);
-    } catch (error) {
-      console.error('Error downloading profile picture:', error);
+        const res = await axios.post('https://i.instagram.com/api/v1/accounts/send_recovery_flow_email/', data, { headers });
+        rest = res.data.email;
+    } catch {
+        rest = 'Band Requests!';
     }
-  }
 
-  const Response = await GetInfoInsta(user);
-  let fols, folg, po, da;
-  if ('id' in Response) {
-    fols = Response.followers;
-    folg = Response.following;
-    po = Response.posts;
-    try {
-      const re = await axios.get(`https://o7aa.pythonanywhere.com/?id=${id}`);
-      da = re.data.date;
-    } catch (error) {
-      da = 'No Date';
+    const user = email.split('@')[0];
+
+    const heada = {
+        "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+        "Host": "i.instagram.com",
+        "Connection": "Keep-Alive",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36",
+        "Cookie": `mid=YwvCRAABAAEsZcmT0OGJdPu3iLUs; csrftoken=${csr}`,
+        "Cookie2": "$Version=1",
+        "Accept-Language": "en-US",
+        "X-IG-Capabilities": "AQ==",
+        "Accept-Encoding": "gzip",
+    };
+
+    const datai = `q=${user}&device_id=android${uid}&guid=${uid}&_csrftoken=${csr}`;
+
+    let kid = await axios.post('https://i.instagram.com/api/v1/users/lookup/', datai, { headers: heada });
+    kid = kid.data;
+
+    let id = kid.user ? kid.user.pk : "";
+    let prv = kid.user ? kid.user.is_private : "";
+    let ph = kid.has_valid_phone || "";
+    let sms = kid.can_sms_reset || "";
+    let fb = kid.fb_login_option || "";
+    let wa = kid.can_wa_reset || "";
+    let phn = kid.obfuscated_phone || "";
+    let name = kid.user ? kid.user.full_name : "";
+    let profile_pic_url = kid.user ? kid.user.profile_pic_url : "";
+
+    let profile_pic_path = `${user}.jpg`;
+    if (profile_pic_url) {
+        const response = await fetch(profile_pic_url);
+        const buffer = await response.buffer();
+        fs.writeFileSync(profile_pic_path, buffer);
     }
-  }
 
-  const tlg = `
+    let tlg = `
 ________main Info_______
 [+] Email ==> ${email}
 [+] E-mail Rest ==> ${rest}
 [+] Username ==> @${user}
 [+] Name ==> ${name}
 [+] ID ==> ${id}
-[+] Followers ==> ${fols}
-[+] Following ==> ${folg}
-[+] Posts ==> ${po}
-[+] Date ==> ${da}
-_______other Info_________    
+[+] Followers ==> followers_count
+[+] Following ==> following_count
+[+] Posts ==> posts_count
+[+] Date ==> created_at
+_______other Info_________
 [+] Is Private ==> ${prv}
-[+] E-mail ==> ${email}
 [+] Has Phone Number ? ==> ${ph}
 [+] SMS Rest ==> ${sms}
 [+] WhatsApp Rest ==> ${wa}
 [+] FaceBook Login ==> ${fb}
 [+] Phone Number ==> ${phn}
-_______BEST Dev_________    
+_______BEST Dev_________
 @maho_s9 - @maho9s
 `;
 
-  console.log(tlg);
-
-  if (fs.existsSync(profile_pic_path)) {
     const form = new FormData();
-    form.append('photo', fs.createReadStream(profile_pic_path));
+    form.append('chat_id', ID);
     form.append('caption', tlg);
+    form.append('photo', fs.createReadStream(profile_pic_path));
 
-    try {
-      await axios.post(`https://api.telegram.org/bot${token}/sendPhoto?chat_id=${ID}`, form, {
+    await axios.post(`https://api.telegram.org/bot${token}/sendPhoto`, form, {
         headers: form.getHeaders()
-      });
-    } catch (error) {
-      console.error('Error sending photo to Telegram:', error);
-    }
-  }
+    });
+};
 
-  rl.close();
-}
-
-main().catch(console.error);
+runBot();
